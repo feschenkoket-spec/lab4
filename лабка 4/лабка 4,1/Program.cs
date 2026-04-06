2f1efef43cd2ee481db78b6f51cdaa320c60ce1f
feschenkoket-spec <feschenkoket@gmail.com>
2026-04-06 13:43:41 +0300

    Add files via upload

---
+++ лабка 4/лабка 4,1/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+class Hour
+{
+    public int value;
+
+    public Hour(int value)
+    {
+        Console.WriteLine("викликано конструктор Hour");
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        Console.WriteLine("викликано Hour.ToString()");
+        return value.ToString("D2");
+    }
+
+    public override bool Equals(object obj)
+    {
+        Console.WriteLine("викликано Hour.Equals()");
+        if (obj is Hour h)
+            return h.value == value;
+        return false;
+    }
+
+    public override int GetHashCode()//повертає хеш код обєкта хаур
+    {
+        Console.WriteLine("викликано Hour.GetHashCode()");
+        return value.GetHashCode();//повертає хеш код числа години // щоб код залежав від знач години
+    }
+}
+
+class Minute
+{
+    public int value;
+
+    public Minute(int value)
+    {
+        Console.WriteLine("викликано конструктор Minute");
+        this.value = value;
+    }
+
+    public override string ToString()
+    {
+        Console.WriteLine("викликано Minute.ToString()");
+        return value.ToString("D2");
+    }
+
+    public override bool Equals(object obj)
+    {
+        Console.WriteLine("викликано Minute.Equals()");
+        if (obj is Minute m)
+            return m.value == value;
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        Console.WriteLine("викликано Minute.GetHashCode()");
+        return value.GetHashCode();
+    }
+}
+
+class Time
+{
+    public Hour hour;
+    public Minute minute;
+
+    public Time(Hour h, Minute m)
+    {
+        Console.WriteLine("викликано конструктор Time");
+        hour = h; //записуємо переданий обєкт у поле
+        minute = m;
+    }
+
+    public void PrintTime()
+    {
+        Console.WriteLine("викликано Time.PrintTime()");
+        Console.WriteLine("поточний час: " + hour.ToString() + ":" + minute.ToString());
+    }
+
+    public override string ToString()
+    {
+        Console.WriteLine("викликано Time.ToString()");
+        return hour.ToString() + ":" + minute.ToString();
+    }
+
+    public override bool Equals(object obj)
+    {
+        Console.WriteLine("викликано Time.Equals()");
+        if (obj is Time t)
+            return hour.Equals(t.hour) && minute.Equals(t.minute);
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        Console.WriteLine("викликано Time.GetHashCode()");
+        return hour.GetHashCode() + minute.GetHashCode();
+    }
+}
+
+class Doba
+{
+    public Time time;
+
+    public Doba(Time t)
+    {
+        Console.WriteLine("викликано конструктор Doba");
+        time = t;
+    }
+
+    public void ShowPartOfDay()
+    {
+        Console.WriteLine("викликано Doba.ShowPartOfDay()");
+        Console.WriteLine("час: " + time.ToString());//виводить час який міститься в обєкті доба
+
+        int h = time.hour.value;//створюємо змінну і записуємо в неї число години
+
+        if (h >= 6 && h < 12)
+            Console.WriteLine("частина доби: ранок");
+        else if (h >= 12 && h < 18)
+            Console.WriteLine("частина доби: день");
+        else if (h >= 18 && h < 23)
+            Console.WriteLine("частина доби: вечір");
+        else
+            Console.WriteLine("частина доби: ніч");
+    }
+
+    public override string ToString()
+    {
+        Console.WriteLine("викликано Doba.ToString()");
+        return "час: " + time.ToString();
+    }
+
+    public override bool Equals(object obj)
+    {
+        Console.WriteLine("викликано Doba.Equals()");
+        if (obj is Doba d)
+            return time.Equals(d.time);
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        Console.WriteLine("викликано Doba.GetHashCode()");
+        return time.GetHashCode();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            DateTime now = DateTime.Now;
+
+            Hour h = new Hour(now.Hour);
+            Minute m = new Minute(now.Minute);
+
+            Time t = new Time(h, m);
+            Doba d = new Doba(t);
+
+            t.PrintTime();
+            d.ShowPartOfDay();
+
+            Console.WriteLine(d.ToString());
+            Console.WriteLine("hash code: " + d.GetHashCode());
+            Console.WriteLine("equals: " + d.Equals(new Doba(t)));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("помилка: " + ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
---
+++ лабка 4/лабка 4,2/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Text;
+
+class RepairOrder
+{
+    private readonly int code;
+    private string description;
+    private double price;
+
+    private static int count = 0;
+
+    public RepairOrder(int code, string description, double price)
+    {
+        this.code = code;
+        this.description = description;
+        this.price = price;
+        count++;
+    }
+
+    public double GetPrice()
+    {
+        return price;
+    }
+
+    public static int GetCount()
+    {
+        return count;
+    }
+
+    public bool IsExpensive()
+    {
+        return price > 2500;
+    }
+
+    public virtual string GetInfo()
+    {
+        return "Код: " + code + ", Опис: " + description + ", Ціна: " + price;
+    }
+}
+
+class PhoneRepair : RepairOrder
+{
+    private string brand;
+
+    public PhoneRepair(int code, string description, double price, string brand)
+        : base(code, description, price)
+    {
+        this.brand = brand;
+    }
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + ", Бренд: " + brand;
+    }
+}
+
+class LaptopRepair : RepairOrder
+{
+    private string problemType;
+
+    public LaptopRepair(int code, string description, double price, string problemType)
+        : base(code, description, price)
+    {
+        this.problemType = problemType;
+    }
+
+    public override string GetInfo()
+    {
+        return base.GetInfo() + ", Тип поломки: " + problemType;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            RepairOrder[] orders = new RepairOrder[3];
+
+            orders[0] = new PhoneRepair(1, "не вмикається", 2000, "Samsung");
+            orders[1] = new LaptopRepair(2, "зламаний екран", 3000, "екран");
+            orders[2] = new PhoneRepair(3, "не заряджається", 2800, "iPhone");
+
+            double total = 0;
+            int expensiveCount = 0;
+
+            for (int i = 0; i < orders.Length; i++)
+            {
+                Console.WriteLine(orders[i].GetInfo());
+                total += orders[i].GetPrice();
+
+                if (orders[i].IsExpensive())
+                {
+                    expensiveCount++;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Загальна сума: " + total);
+            Console.WriteLine("Кількість дорогих ремонтів: " + expensiveCount);
+            Console.WriteLine("Всього заявок: " + RepairOrder.GetCount());
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Помилка: " + ex.Message);
+        }
+
+        Console.ReadKey();
+    }
+}
